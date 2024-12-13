aaedbedcb2606b01c0d60cf28796e133b98cec45
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-12-13 23:14:27 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import Button from './components/button/Button';
+// import Button from './components/button/Button';
+import { USERS } from './constants/infoUsers';
+import { useState } from 'react';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => {
+	const [userAccount, setUserAccount] = useState(0);
 	return (
 		<div>
 			<GlobalStyles />
-			<h1>React</h1>
-			<Button color='steelblue'>Click Me!</Button>
-			<Button color='#f0f'>Click Me!</Button>
+			<img src={USERS[userAccount].profileImage} alt='' />
+			<h1>
+				Name: {USERS[userAccount].title} {USERS[userAccount].name}
+			</h1>
+			<p>Username: {USERS[userAccount].username}</p>
+			<p>Email: {USERS[userAccount].email}</p>
+			<button
+				disabled={userAccount === 0}
+				onClick={() => {
+					if (userAccount > 0) {
+						setUserAccount(userAccount - 1);
+					}
+				}}
+			>
+				Previous
+			</button>
+			<button
+				disabled={userAccount >= USERS.length - 1}
+				onClick={() => {
+					if (userAccount < USERS.length - 1) {
+						setUserAccount(userAccount + 1);
+					}
+				}}
+			>
+				Next
+			</button>
 		</div>
 	);
 };
